297b982fadabf0315d22e1f351afd1c409a6051c
tom <tom@sfxcode.com>
2019-06-23 18:38:35 +0200

    added background options

--- plugin/src/main/scala/com/sfxcode/paradox/reveal/markdown/directive/SlideDirective.scala
+++ plugin/src/main/scala/com/sfxcode/paradox/reveal/markdown/directive/SlideDirective.scala
@@ -11,17 +11,27 @@ case class SlideDirective(name: String) extends ContainerBlockDirective(Array(na
 
     val id = node.attributes.value("id", "")
     val styleclass = node.attributes.value("class", "")
+    val style = node.attributes.value("style", "")
 
     val transition = node.attributes.value("transition", "")
     val transitionSpeed = node.attributes.value("transitionSpeed", "")
 
-    val background = node.attributes.value("background", "")
-    val backgroundRepeat = node.attributes.booleanValue("backgroundRepeat", false)
-    val backgroundSize = node.attributes.value("backgroundSize", "")
-    val backgroundTransition = node.attributes.value("backgroundTransition", "")
+    val color = node.attributes.value("color", "")
+
+    val image = node.attributes.value("image", "")
 
     val video = node.attributes.value("video", "")
+    val videoLoop = video.nonEmpty && node.attributes.booleanValue("loop", false)
+    val videoMuted = video.nonEmpty && node.attributes.booleanValue("muted", false)
+
     val iframe = node.attributes.value("iframe", "")
+    val iframeInteractive = iframe.nonEmpty && node.attributes.booleanValue("interactive", false)
+
+    val backgroundSize = node.attributes.value("size", "")
+    val backgroundPosition = node.attributes.value("position", "")
+    val backgroundRepeat = node.attributes.value("repeat", "")
+    val backgroundOpacity = node.attributes.value("opacity", "")
+    val backgroundTransition = node.attributes.value("backgroundTransition", "")
 
     printer.println()
 
@@ -30,8 +40,12 @@ case class SlideDirective(name: String) extends ContainerBlockDirective(Array(na
     if (id.nonEmpty)
       printer.print(s""" id="${id}"""")
 
+    if (style.nonEmpty)
+      printer.print(s""" id="${style}"""")
+
     if (styleclass.nonEmpty)
       printer.print(s""" id="${styleclass}"""")
+
 
     if (transition.nonEmpty && TransitionList.contains(transition.toLowerCase))
       printer.print(s""" data-transition="${transition.toLowerCase}"""")
@@ -39,23 +53,42 @@ case class SlideDirective(name: String) extends ContainerBlockDirective(Array(na
     if (transitionSpeed.nonEmpty && TransitionSpeedList.contains(transitionSpeed.toLowerCase))
       printer.print(s""" data-transition-speed="${transitionSpeed.toLowerCase}"""")
 
-    if (background.nonEmpty)
-      printer.print(s""" data-background="${background}"""")
 
-    if (backgroundRepeat)
-      printer.print(s""" data-background-repeat="repeat"""")
+    if (color.nonEmpty)
+      printer.print(s""" data-background="${color}"""")
+
+    if (video.nonEmpty)
+      printer.print(s""" data-background-color="${video}"""")
+
+    if (videoLoop)
+      printer.print(s"""  data-background-video-loop""")
+
+
+    if (iframe.nonEmpty)
+      printer.print(s""" data-background-iframe="${iframe}"""")
+
+    if (iframeInteractive)
+      printer.print(s"""  data-background-interactive""")
+
+
+    if (image.nonEmpty)
+      printer.print(s""" data-background="${image}"""")
 
     if (backgroundSize.nonEmpty)
-      printer.print(s""" data-background-Size="${backgroundSize}"""")
+      printer.print(s""" data-background-size="${backgroundSize}"""")
+
+    if (backgroundRepeat.nonEmpty)
+      printer.print(s""" data-background-repeat="${backgroundRepeat}"""")
+
+    if (backgroundPosition.nonEmpty)
+      printer.print(s""" data-background-position="${backgroundPosition}"""")
+
+    if (backgroundOpacity.nonEmpty)
+      printer.print(s""" data-background-opacity="${backgroundOpacity}"""")
 
     if (backgroundTransition.nonEmpty && TransitionList.contains(transition.toLowerCase))
       printer.print(s""" data-background-transition="${backgroundTransition.toLowerCase}"""")
 
-    if (video.nonEmpty)
-      printer.print(s""" data-background-video="${background}"""")
-
-    if (iframe.nonEmpty)
-      printer.print(s""" data-background-iframe="${iframe}" data-background-interactive""")
 
     printer.print(s""">""")
     node.contentsNode.accept(visitor)
